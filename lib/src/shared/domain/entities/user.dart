5201f5ca9e0cb80d1df8bef752d1f30cf6ba214a
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2024-11-07 03:03:08 -0400

    Refactor use of User for manage accounts

---
+++ lib/src/shared/domain/entities/user.dart
@@ -0,0 +1,20 @@
+
+class  User  {
+  final String userName;
+  final String userEmail;
+  final String userRole;
+  final String userImgUrl;
+
+  User(
+        {
+          required  this .userName,
+          required  this .userEmail,
+          required  this .userRole,
+          required  this .userImgUrl,
+        }
+      );
+
+  String getImg(){
+    return userImgUrl;
+  }
+}
